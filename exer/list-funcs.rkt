c91cc45bf2abc1c76645030337f7f496f8329b8e
Suvankar Mazumder <smazumder@gmail.com>
2016-12-13 01:33:48 -0500

    Update list-funcs.rkt

--- exer/list-funcs.rkt
+++ exer/list-funcs.rkt
@@ -9,6 +9,14 @@
 ; Define append using fold
 (define (append-t l1 l2)
   (foldr cons l2 l1))
+  
+; Define append using pattern matching
+(define (append-2 L1 L2)
+  (match L1
+    ['() L2]
+    [`(,head . ,tail)
+     `(,head . ,(append-2 tail L2))]))
 
 (append-r '( 1 2 3) '(4 5 6))
 (append-t '( 1 2 3 7) '(4 5 6))
+(append-2 '( 1 2 3 7) '(4 5 6))
